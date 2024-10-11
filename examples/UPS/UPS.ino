bf3f51d70f623b19782aebd96a8e58b747df3309
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-10-11 20:30:11 +0200

    Remove HID_PD_AUDIBLEALARMCTRL (AudibleAlarmControl) to simplify

--- examples/UPS/UPS.ino
+++ examples/UPS/UPS.ino
@@ -32,7 +32,6 @@ uint16_t iRemainTimeLimit = 600;
 int16_t  iDelayBe4Reboot = -1;
 int16_t  iDelayBe4ShutDown = -1;
 uint16_t iManufacturerDate = 0; // initialized in setup function
-byte iAudibleAlarmCtrl = 2; // 1 - Disabled, 2 - Enabled, 3 - Muted
 
 
 // Parameters for ACPI compliancy
@@ -80,8 +79,6 @@ void setup() {
 
   PowerDevice.setStringFeature(HID_PD_IDEVICECHEMISTRY, &bDeviceChemistry, STRING_DEVICECHEMISTRY);
   PowerDevice.setStringFeature(HID_PD_IOEMINFORMATION, &bOEMVendor, STRING_OEMVENDOR);
-
-  PowerDevice.setFeature(HID_PD_AUDIBLEALARMCTRL, &iAudibleAlarmCtrl, sizeof(iAudibleAlarmCtrl));
 
   PowerDevice.setFeature(HID_PD_DESIGNCAPACITY, &iDesignCapacity, sizeof(iDesignCapacity));
   PowerDevice.setFeature(HID_PD_FULLCHRGECAPACITY, &iFullChargeCapacity, sizeof(iFullChargeCapacity));
