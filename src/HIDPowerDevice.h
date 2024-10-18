b582cfa943cd359aa6b9c8a716fbc73e50d84cb2
Fredrik Orderud <forderud@gmail.com>
2024-10-18 11:05:51 +0200

    Remove unused HID_PD_CURRENT define.

--- src/HIDPowerDevice.h
+++ src/HIDPowerDevice.h
@@ -50,7 +50,6 @@
 #define HID_PD_VOLTAGE               0x0B // 11 INPUT (NA) OR FEATURE(implemented)
 #define HID_PD_REMAININGCAPACITY     0x0C // 12 INPUT OR FEATURE(required by Windows)
 #define HID_PD_FULLCHRGECAPACITY     0x0E // 14 FEATURE ONLY. Last Full Charge Capacity 
-#define HID_PD_CURRENT               0x15 // 21 FEATURE ONLY
 #define HID_PD_CAPACITYMODE          0x16
 #define HID_PD_DESIGNCAPACITY        0x17
 #define HID_PD_AVERAGECURRENT        0x1B
