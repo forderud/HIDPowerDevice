75cd5cd461cc092c95a8f96d35e86a3fde5efae9
Fredrik Orderud <forderud@gmail.com>
2024-10-21 19:33:37 +0200

    Remove BatteryPresent flag to simplify.

--- examples/UPS/UPS.ino
+++ examples/UPS/UPS.ino
@@ -89,10 +89,6 @@ void loop() {
     iPresentStatus.RemainingTimeLimitExpired = 0;
   }
   
-  iPresentStatus.BatteryPresent = 1;
-
-  
-
   //************ Delay ****************************************  
   delay(1000);
   iIntTimer++;
