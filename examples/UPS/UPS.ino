49ab982ee09d0fc58f49ed73fff70dff64d63980
Fredrik Orderud <forderud@gmail.com>
2024-10-26 20:22:06 +0200

    TEST: Prove that the battery still appear in device manager, even without sending a single report.

--- examples/UPS/UPS.ino
+++ examples/UPS/UPS.ino
@@ -1,108 +1,12 @@
 #include <HIDPowerDevice.h>
 
-#define MINUPDATEINTERVAL   26
-#define CHGDCHPIN           4
-#define RUNSTATUSPIN        5
-#define COMMLOSTPIN         10
-#define BATTSOCPIN          A7
-
-int iIntTimer=0;
-
-PresentStatus iPresentStatus = {}, iPreviousStatus = {};
-
-byte bCapacityMode = 1;  // unit: 0=mAh, 1=mWh, 2=%
-
-// Physical parameters
-uint16_t iVoltage =1499; // centiVolt
-
-// Parameters for ACPI compliancy
-const uint32_t iDesignCapacity = 58003*360/iVoltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
-uint32_t iFullChargeCapacity = 40690*360/iVoltage; // AmpSec=mWh*360/centiVolt (1 mAh = 3.6 As)
-
-uint32_t iRemaining =0, iPrevRemaining=0;
-bool bCharging = false;
-
-int iRes=0;
-
-
 void setup() {
-
   Serial.begin(57600);
   
   // Used for debugging purposes. 
   PowerDevice.setOutput(Serial);
-  
-  pinMode(CHGDCHPIN, INPUT_PULLUP); // ground this pin to simulate power failure. 
-  pinMode(RUNSTATUSPIN, OUTPUT);  // output flushing 1 sec indicating that the arduino cycle is running. 
-  pinMode(COMMLOSTPIN, OUTPUT); // output is on once communication is lost with the host, otherwise off.
-
-
-  PowerDevice.setFeature(HID_PD_PRESENTSTATUS, &iPresentStatus, sizeof(iPresentStatus));
-  
-  PowerDevice.setFeature(HID_PD_CAPACITYMODE, &bCapacityMode, sizeof(bCapacityMode));
-  PowerDevice.setFeature(HID_PD_VOLTAGE, &iVoltage, sizeof(iVoltage));
-
-  PowerDevice.setFeature(HID_PD_DESIGNCAPACITY, &iDesignCapacity, sizeof(iDesignCapacity));
-  PowerDevice.setFeature(HID_PD_FULLCHRGECAPACITY, &iFullChargeCapacity, sizeof(iFullChargeCapacity));
-  PowerDevice.setFeature(HID_PD_REMAININGCAPACITY, &iRemaining, sizeof(iRemaining));
 }
 
 void loop() {
-  //*********** Measurements Unit ****************************
-  int iBattSoc = analogRead(BATTSOCPIN); // potensiometer value in [0,1024)
-
-  iRemaining = (uint32_t)(round((float)iFullChargeCapacity*iBattSoc/1024));
-
-  if (iRemaining > iPrevRemaining)
-    bCharging = true;
-  else if (iRemaining < iPrevRemaining)
-    bCharging = false;
-  
-  // Charging
-  iPresentStatus.Charging = bCharging;
-  iPresentStatus.ACPresent = bCharging; // assume charging implies AC present
-    
-  // Discharging
-  if(!bCharging) { // assume not charging implies discharging
-    iPresentStatus.Discharging = 1;
-  }
-  else {
-    iPresentStatus.Discharging = 0;
-  }
-  
-  //************ Delay ****************************************  
   delay(1000);
-  iIntTimer++;
-  digitalWrite(RUNSTATUSPIN, HIGH);   // turn the LED on (HIGH is the voltage level);
-  delay(1000);
-  iIntTimer++;
-  digitalWrite(RUNSTATUSPIN, LOW);   // turn the LED off;
-
-  //************ Check if we are still online ******************
-
-  
-
-  //************ Bulk send or interrupt ***********************
-
-  if((iPresentStatus != iPreviousStatus) || (iRemaining != iPrevRemaining) || (iIntTimer>MINUPDATEINTERVAL) ) {
-
-    PowerDevice.sendReport(HID_PD_REMAININGCAPACITY, &iRemaining, sizeof(iRemaining));
-    iRes = PowerDevice.sendReport(HID_PD_PRESENTSTATUS, &iPresentStatus, sizeof(iPresentStatus));
-
-    if(iRes <0 ) {
-      digitalWrite(COMMLOSTPIN, HIGH);
-    }
-    else
-      digitalWrite(COMMLOSTPIN, LOW);
-        
-    iIntTimer = 0;
-    iPreviousStatus = iPresentStatus;
-    if (abs(iPrevRemaining - iRemaining) > 1) // add a bit of hysteresis
-      iPrevRemaining = iRemaining;
-  }
-  
-
-  Serial.println(iRemaining);
-  Serial.println(iRes);
-  
 }
